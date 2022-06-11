0538dd06860eb8b4ad95e3e9f3c53d9250489733
Charlotte <charlotte.kj.roe@gmail.com>
2022-06-11 21:30:31 +0100

    Finished accordian element

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// array of objects containing accordian content
 let programme = [
   {
     heading:
@@ -6,19 +5,23 @@ let programme = [
     content: [
       {
         title:
-          "OPENING LIVE PERFORMANCE : Sunday 10th July, FERVĒRE (2022) by Una Lee",
-        description: "",
+          "<h3>OPENING LIVE PERFORMANCE : Sunday 10th July, FERVĒRE (2022) by Una Lee</h3>",
+        description:
+          "<p>Completing her mini residency at dai hall, Lee will present site-specific piece FERVĒRE, informed by walls and air in the space.</p>",
       },
       {
-        title: "Exhibition : waste ethos – recycle, regenerate, repurpose",
-        description: "",
+        title:
+          "<h3>Exhibition : waste ethos – recycle, regenerate, repurpose</h3>",
+        description:
+          "<p>Group exhibition by Anne-F Jacques, Yonago Tadashi, Muku Kobayashi & Bunchi in collaboration with Octopus Collective.</p>",
       },
       {
-        title: "Thurs 13th & Fri 14th July: FREE Graffiti Workshops",
-        description: "",
+        title: "<h3>Thurs 13th & Fri 14th July: FREE Graffiti Workshops</h3>",
+        description:
+          "<p>Come down to a 2-day graffiti workshop at dai hall! </p>",
       },
     ],
-    link: "",
+    link: '<a href="#" target="_blank">Read more...</a>',
     open: false,
   },
   {
@@ -27,23 +30,27 @@ let programme = [
     content: [
       {
         title:
-          "Film Screening 17:00 - 18:00: Bohman Brothers : By Biro And Umbrella Spring (2016 ‧ Documentary ‧ 1h ‧ Tyler Hubby, Cathy Soreny)",
-        description: "",
+          "<h3>Film Screening 17:00 - 18:00: Bohman Brothers : By Biro And Umbrella Spring (2016 ‧ Documentary ‧ 1h ‧ Tyler Hubby, Cathy Soreny)</h3>",
+        description:
+          "<p>“Scavenger improv artisan, junk mail poet and scrapbook illustrator – Cathy Soreny’s documentary on Adam Bohman is an essential portrait of one of Britain’s most idiosyncratic and quietly prolific artists.” - Peter Strickland, Director</p>",
       },
       {
-        title: "19:00 - 19:45 Arthur Chambry",
-        description: "",
+        title: "<h3>19:00 - 19:45 Arthur Chambry</h3>",
+        description:
+          "<p>LÀ OÙ L’AIR FROTTE “Soon, the world will become a big sound box and your legs will be two heavy mallets. This cadence that made us feel comfortable now appears as an old story. This warm feeling of familiarity irritates you. We have to look for a new balance. Build some opposite worlds, make your teeth rattle, snap your fingers, each impact is a clue about the birth of the next gesture. Organise them. Each impact is a punch and now you need to strike hard. Paint the surfaces with a scream, no matter what its quality, it will fragment into echoes and will dissolve. Breath hard and blow but don’t stop walking. The skin behind your heels becomes hard and your feet are swollen, the pain is not negotiable, it is resonating.” </p>",
       },
       {
-        title: "20:00 - 20:45 Bohman Brothers",
-        description: "",
+        title: "<h3>20:00 - 20:45 Bohman Brothers</h3>",
+        description: "<p></p>",
       },
       {
-        title: "21:00 - 21:45 Elaine Mitchener + Audrey Chen + Mariam Rezaei",
-        description: "",
+        title:
+          "<h3>21:00 - 21:45 Elaine Mitchener + Audrey Chen + Mariam Rezaei</h3>",
+        description:
+          "<p>Audrey Chen, Elaine Mitchener and Mariam Rezaei will be performing a free improv set as a trio.</p>",
       },
     ],
-    link: "",
+    link: '<a href="#" target="_blank">Go to full programme for the day.</a>',
     open: false,
   },
   {
@@ -52,62 +59,62 @@ let programme = [
     content: [
       {
         title:
-          "Film Screening 17:00 - 18:30: Sonita (2015 ‧ PG ‧ 1h 30m ‧ Rokhsareh Ghaemmaghami)",
-        description: "",
+          "<h3>Film Screening 17:00 - 18:30: Sonita (2015 ‧ PG ‧ 1h 30m ‧ Rokhsareh Ghaemmaghami)</h3>",
+        description:
+          "<p>After her family attempts to sell her into marriage, a young Afghan refugee in Iran channels her frustrations and seizes her destiny through music. Grabbing the mic, she spits fiery rhymes in the face of oppressive traditions.</p>",
       },
       {
-        title: "19:00 - 19:45 The Heads and The Golds",
-        description: "",
+        title: "<h3>19:00 - 19:45 The Heads and The Golds</h3>",
+        description:
+          "<p>The couple behind shared projects including The Hunter Gracchus and Singing Knives Records and individual projects such as Roman Nose, Blue Yodel and Papal Bull, Fiona Kennedy and Jon Marshall’s duo work draws on their professional and personal experiences of mental health as manifested in the individual and the family and set against contemporary depoliticised, pathologizing and commodified interventions, playing with disrupted sound localization and attempts to determine and categorise acousmatic and visualised sound sources.</p>",
       },
       {
-        title: "20:00 - 20:45 Ji Youn Kang",
-        description: "",
+        title: "<h3>20:00 - 20:45 Ji Youn Kang</h3>",
+        description:
+          "<p>Kang plays with two customized bamboos and a kkwaenggwari, with analog devices that connect the instruments for further real-time processing. She uses the voltage flow between her body, a self-built generator, the bamboo sticks and an extra synth, until they become strongly linked together. The kkwaenggwari provides rhythmical ritual flow created by feedback system equipped hidden, and takes over the gesture from the bamboos, expanding its rhythmic worlds outwards, until the whole space becomes a part of the resonating body, singing and breathing together with the instruments’ stubborn struggling voices.</p>",
       },
       {
-        title: "21:00 - 21:45 Mazen Kerbaj",
-        description: "",
+        title: "<h3>21:00 - 21:45 Mazen Kerbaj</h3>",
+        description:
+          "<p>Playing in solo has always been an important part of Mazen Kerbaj’s musical journey; it is in this bare-bones setup that he can experiment the most with his instrument, pushing it beyond any recognition. His use of various every-day objects (tubes, plastic bowls, balloons, aluminium foil…) to “prepare” the trumpet, and his unique playing techniques allow him to create raw soundscapes that sound sometimes like electronics (he himself describes his playing as “electronic music, played acoustically”), especially when he plays several continuous layers of sound at the same time on an instrument that is not designed for that.</p>",
       },
     ],
-    link: "",
+    link: '<a href="#" target="_blank">Go to full programme for the day.</a>',
     open: false,
   },
   {
     heading:
-      "<h2>Sunday 17th<h2><h3>Film screening and performances</h3><p>Tony Conrad : completely in the present (2016), NeAnoNeNoNo (Jorge Boehringer + Lucy Cheesman + Stephen Chase + Theo Gowans), Leafcutter John, Lau Nau<p>",
+      "<h3>Sunday 17th<h2><h3>Film screening and performances</h3><p>Tony Conrad : completely in the present (2016), NeAnoNeNoNo (Jorge Boehringer + Lucy Cheesman + Stephen Chase + Theo Gowans), Leafcutter John, Lau Nau</h3>",
     content: [
       {
         title:
-          "Film Screening 17:00 - 18:30: Tony Conrad : completely in the present (2016 ‧ Documentary ‧ 1h 36m ‧ Tyler Hubby)",
-        image: "",
-        description: "",
-        bios: "",
+          "<h3>Film Screening 17:00 - 18:30: Tony Conrad : completely in the present (2016 ‧ Documentary ‧ 1h 36m ‧ Tyler Hubby)<h3>",
+        description:
+          "<p>Feature documentary on the pioneering life and work of iconoclastic filmmaker/musician/composer/artist Tony Conrad.</p>",
       },
       {
         title:
-          "19:00 - 19:45 NeAnoNeNoNo (Jorge Boehringer + Lucy Cheesman + Stephen Chase + Theo Gowans)",
-        image: "",
-        description: "",
-        bios: "",
+          "<h3>19:00 - 19:45 NeAnoNeNoNo (Jorge Boehringer + Lucy Cheesman + Stephen Chase + Theo Gowans)</h3>",
+        description:
+          "<p>NeAnoNeNoNo is a new publication concern, and this performance will celebrate a new issue of the zine with that name.  Most of the characters you will meet in this performance have work in this ‘paginated exhibition’, and maybe someday they all will, maybe all of you will too</p>",
       },
       {
-        title: "20:00 - 20:45 Leafcutter John",
-        image: "",
-        description: "",
-        bios: "",
+        title: "<h3>20:00 - 20:45 Leafcutter John</h3>",
+        description:
+          "<p> ‘Gritstone’ combines John's new found passion for rock climbing and his popular audio software Forester. The performance focuses on the material qualities of Gritstone, which was quarried in the Peaks for use in milling flour and grinding blades. The gritstone edges of the Peaks are now a much loved climbing areas. During the performance John will create sounds using pieces of gritstone as well as using field recordings he has made across the Peak District. </p>",
       },
       {
-        title: "21:00 - 21:45 Lau Nau",
-        image: "",
-        description: "",
-        bios: "",
+        title: "<h3>21:00 - 21:45 Lau Nau</h3>",
+        description:
+          "<p>Lau Nau plays a solo concert with modular synthesizer, vocals and field recordings, creating a soundscape of the island where she lives.</p>",
       },
     ],
-    link: "",
+    link: '<a href="#" target="_blank">Go to full programme for the day.</a>',
     open: false,
   },
 ];
 
-// select 'faqs' div from index.html
+// select 'programme' div from index.html
 const programme_element = document.querySelector(".programme");
 
 programme.map(function (item) {
@@ -132,14 +139,18 @@ programme.map(function (item) {
 
   item_element.appendChild(item_heading_element);
 
-  // map for content elements
-  item.content.map(function (event) {
-    const item_content_element = document.createElement("div");
-    item_content_element.classList.add("content");
-    item_content_element.innerHTML = event.title;
+  const item_content_element = document.createElement("div");
+  item_content_element.classList.add("content");
+  item_content_element.innerHTML =
+    item.content
+      .map(function (event) {
+        const item_content_sub_element = document.createElement("div");
+        item_content_sub_element.classList.add("content");
+        item_content_sub_element.innerHTML = event.title + event.description;
 
-    item_element.appendChild(item_content_element);
-  });
-
+        item_element.appendChild(item_content_sub_element);
+      })
+      .join(" ") + item.link;
+  item_element.appendChild(item_content_element);
   programme_element.appendChild(item_element);
 });
